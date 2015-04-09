36b1d2137bcb0794941ed38b2353b3443dc34c11
radekg <radek@gruchalski.com>
2015-04-09 18:49:16 +0200

    Move common components to core. Fixes #13.

--- src/main/java/com/gossiperl/client/exceptions/GossiperlClientException.java
+++
@@ -1,8 +0,0 @@
-package com.gossiperl.client.exceptions;
-
-public class GossiperlClientException extends Exception {
-    public GossiperlClientException() {}
-    public GossiperlClientException(String message) { super(message); }
-    public GossiperlClientException(Throwable cause) { super(cause); }
-    public GossiperlClientException(String message, Throwable cause) { super(message, cause); }
-}
